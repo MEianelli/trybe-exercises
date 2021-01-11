e3a7887f17f89df45a570ac60bce959650ce5855
MEianelli <filipelouzano@gmail.com>
2021-01-11 16:00:40 -0300

    Fim exercicios 11.1

--- exercises/11.1/hello-world/src/App.js
+++ exercises/11.1/hello-world/src/App.js
@@ -1,7 +1,11 @@
 import NameAndDescription from './Component';
 
 function App() {
-  return <NameAndDescription />;
+  const compromissos = ['musica', 'exercicios', 'comer', 'jogar', 'dormir'];
+  const task = value => {
+    return <li>{value}</li>;
+  };
+  return compromissos.map(task);
 }
 
 export default App;
